063b6fd7fa6a461f82c46cace65659161c7887dc
侯斌 <myvstar@gmail.com>
2015-08-09 22:44:34 +0900

    Init project

---
+++ envflag.go
@@ -0,0 +1,108 @@
+package envflag
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Config provides configuration for this package
+type Config struct {
+	// DebugEnabled enables debug mode
+	DebugEnabled bool
+	// MinLength defines min length of flag key, in order to support shortcut
+	MinLength int
+	// EnvFlagDict is a user-defined env-flag map
+	EnvFlagDict map[string]string
+}
+
+// config is a default configuration of envflag package
+var config = &Config{
+	DebugEnabled: false,
+	MinLength:    3,
+	EnvFlagDict:  map[string]string{},
+}
+
+// Setup sets up this package with customized config
+func Setup(c *Config) {
+	config = c
+}
+
+// envToFlag converts THIS_FORMAT to this-format
+func envToFlag(e string) string {
+	return strings.Replace(strings.ToLower(e), "_", "-", -1)
+}
+
+// flagToEnv converts this-format to THIS_FORMAT
+func flagToEnv(f string) string {
+	return strings.Replace(strings.ToUpper(f), "-", "_", -1)
+}
+
+// Parse parses the command-line flags from env and os.Args[1:].
+// Value from env can be overrided by os.Args[1:].
+// This function also add ENVIRONMENT VARIABLE to usage.
+// It is extremely recommended to call this function in main()
+// after all flags are defined and before flags are accessed by the program.
+func Parse() {
+	if flag.Parsed() {
+		return
+	}
+
+	flagEnvMap := map[string]string{}
+	for k, v := range config.EnvFlagDict {
+		flagEnvMap[v] = k
+	}
+	// Rewrite flag.Useage
+	flag.VisitAll(func(f *flag.Flag) {
+		if len(f.Name) < config.MinLength {
+			return
+		}
+		envKey, ok := flagEnvMap[f.Name]
+		if !ok {
+			envKey = flagToEnv(f.Name)
+		}
+		f.Usage = fmt.Sprintf("[%s] %s", envKey, f.Usage)
+	})
+
+	for _, envLine := range os.Environ() {
+		debug("Find a new line of environment variable, ", envLine)
+		envKV := strings.SplitN(envLine, "=", 2)
+		var key, value string
+		key = envKV[0]
+		if len(key) < config.MinLength {
+			continue
+		}
+		if len(envKV) > 1 {
+			value = envKV[1]
+		} else {
+			value = ""
+		}
+
+		var flagKey string
+		if userFlag, ok := config.EnvFlagDict[key]; ok {
+			flagKey = userFlag
+		} else {
+			flagKey = envToFlag(key)
+		}
+		debug("ENV ", key, " is converted to ", flagKey)
+		if flag.Lookup(flagKey) == nil {
+			debug(flagKey, " is not defined in flag, skip!")
+			continue
+		}
+
+		debug("Set  [", flagKey, ",", value, "] to flag")
+		if err := flag.Set(flagKey, value); err != nil {
+			log.Printf("error when set [%s,%s] into flag\n", flagKey, value)
+		}
+	}
+	flag.Parse()
+}
+
+func debug(v ...interface{}) {
+	if !config.DebugEnabled {
+		return
+	}
+	log.Println(v)
+}
---
+++ example/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/kouhin/envflag"
+)
+
+func main() {
+	var (
+		databaseMasterHost = flag.String("database-master-host", "", "Database master host")
+		databaseMasterPort = flag.Int("database-master-username", -1, "Database master port")
+		mh                 = flag.String("mh", "", "a shortcut for database-master-host")
+	)
+	envflag.Parse()
+	fmt.Println("RESULT: ", *databaseMasterHost, ":", *databaseMasterPort, mh)
+}
